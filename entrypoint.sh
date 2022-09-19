caf3d472fe0f5e60deea47c65eca3c1ce936a3e5
Daniel Ciborowski <dciborow@microsoft.com>
2022-09-19 18:07:27 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,15 +10,8 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 export REVIEWDOG_VERSION=v0.14.1
 
-echo "[action-pylint] Installing reviewdog..."
+echo "[action-armttk] Installing reviewdog..."
 wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b /tmp "${REVIEWDOG_VERSION}"
-
-if [[ "$(which pylint)" == "" ]]; then
-  echo "[action-pylint] Installing pylint package..."
-  python -m pip install --upgrade pylint
-fi
-echo "[action-pylint] pylint version:"
-pylint --version
 
 if [[ "$INPUT_PYLINT_RC" == "" ]]; then
   # Do not supply the rcfile option if it is not provided
@@ -27,10 +20,10 @@ else
   rcfile_option="--rcfile=\"${INPUT_PYLINT_RC}\""
 fi
 
-echo "[action-pylint] Checking python code with the pylint linter and reviewdog..."
+echo "[action-armttk] Checking armttk output with reviewdog..."
 exit_val="0"
 
-pylint --score n ${rcfile_option} ${INPUT_PYLINT_ARGS} ${INPUT_GLOB_PATTERN} 2>&1 | # Removes ansi codes see https://github.com/reviewdog/errorformat/issues/51
+echo ./armttk.xml 2>&1 | # Removes ansi codes see https://github.com/reviewdog/errorformat/issues/51
   /tmp/reviewdog -efm="%f:%l:%c: %m" \
     -name="${INPUT_TOOL_NAME}" \
     -reporter="${INPUT_REPORTER}" \
@@ -39,7 +32,7 @@ pylint --score n ${rcfile_option} ${INPUT_PYLINT_ARGS} ${INPUT_GLOB_PATTERN} 2>&
     -level="${INPUT_LEVEL}" \
     ${INPUT_REVIEWDOG_FLAGS} || exit_val="$?"
 
-echo "[action-pylint] Clean up reviewdog..."
+echo "[action-armttk] Clean up reviewdog..."
 rm /tmp/reviewdog
 
 if [[ "${exit_val}" -ne '0' ]]; then
